25ff833a59968ef3ffd6492d6899c8654f59818a
Peter Snoek <petersnoek@davinci.nl>
2015-03-24 13:54:02 +0100

    Removed Identity authentication / account manage things; added basecontroller to fill ViewBag.MenuItems; added menuitems to shared layout

--- src/Bigrivers.Server/Bigrivers.Server.Model/MenuItem.cs
+++ src/Bigrivers.Server/Bigrivers.Server.Model/MenuItem.cs
@@ -13,16 +13,11 @@ namespace Bigrivers.Server.Model
     {
         public int Id { get; set; }
         public string DisplayName { get; set; }
+        public string URL { get; set; }
         public int Order { get; set; }
         public int Parent { get; set; }
 
         [DefaultValue(true)]
         public bool Status { get; set; }
-        public MenuItemType MenuItemType { get; set; }
-        public string Content { get; set; }
-
-        // ExternURL:   "http://www.crowdfunding.com/bigrivers"
-        // FileURL:     "/files/programma.pdf" ;  dit wijst naar een plek op de bigrivers webserver waar alle downloadbare files staan
-        // Page:        "http://www.bigrivers.nl/page</id>" => "[event(2)] <br> Dit is een mooi evenement. De volgende artiesten komen daar: <br> [event(2)\artists]"
-    }
+     }
 }
