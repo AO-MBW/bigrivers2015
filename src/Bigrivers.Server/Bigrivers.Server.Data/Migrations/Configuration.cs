596968aedc7479a0d07921159d5d30ef7f99aa8d
Andreas Furster <andreas.furster@hotmail.com>
2015-05-18 22:50:21 +0200

    Don't insert shitdata if you build on release

--- src/Bigrivers.Server/Bigrivers.Server.Data/Migrations/Configuration.cs
+++ src/Bigrivers.Server/Bigrivers.Server.Data/Migrations/Configuration.cs
@@ -18,6 +18,7 @@ namespace Bigrivers.Server.Data.Migrations
 
         protected override void Seed(BigriversDb context)
         {
+#if DEBUG
             context.Genres.RemoveRange(context.Genres);
             context.Locations.RemoveRange(context.Locations);
             context.Artists.RemoveRange(context.Artists);
@@ -155,6 +156,7 @@ namespace Bigrivers.Server.Data.Migrations
             context.Locations.Add(loc);
 
             context.SaveChanges();
+#endif
         }
     }
 }
