5e4d53b56930c1c9d3284a2c44315d410f9bc72f
rikproDVC <99016312@mydavinci.nl>
2015-06-02 14:32:09 +0200

    Updated webapplication project file to include bootstrap css/js

--- src/Bigrivers.Client/Bigrivers.Client.WebApplication/Scripts/_references.js
+++ src/Bigrivers.Client/Bigrivers.Client.WebApplication/Scripts/_references.js
@@ -5,3 +5,7 @@
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="respond.js" />
+/// <reference path="jquery-2.1.3.js" />
+/// <reference path="modernizr-2.8.3.js" />
+/// <reference path="npm.js" />
+/// <reference path="respond.matchmedia.addlistener.js" />
