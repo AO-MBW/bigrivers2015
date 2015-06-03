7bf7b427844310ba72f9388d84afd2fbe1229c52
rikproDVC <99016312@mydavinci.nl>
2015-06-03 14:12:59 +0200

    Created Widget Item models and backend controller+views; Created location backend controller+views;

---
+++ src/Bigrivers.Server/Bigrivers.Server.Model/WidgetItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Bigrivers.Server.Model
+{
+    public class WidgetItem
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public int? Order { get; set; }
+
+        [DefaultValue(true)]
+        public bool Status { get; set; }
+
+        [DefaultValue(false)]
+        public bool Deleted { get; set; }
+
+        public virtual Link Target { get; set; }
+        public virtual File Image { get; set; }
+    }
+}
