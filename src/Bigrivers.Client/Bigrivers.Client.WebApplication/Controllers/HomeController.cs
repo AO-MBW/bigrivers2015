1154835b7599446b3c4ad93d8af60e33d107ccae
NielsKlop <niels_klop@hotmail.com>
2015-06-05 13:53:43 +0200

    Added news widget and diferrent footer background

--- src/Bigrivers.Client/Bigrivers.Client.WebApplication/Controllers/HomeController.cs
+++ src/Bigrivers.Client/Bigrivers.Client.WebApplication/Controllers/HomeController.cs
@@ -28,11 +28,17 @@ namespace Bigrivers.Client.WebApplication.Controllers
         public ActionResult Index()
         {
             ViewBag.ButtonItems = AccessLayer.ButtonItems.Where(m => m.Status).ToList();
-
+            
             ViewBag.NewsItems = AccessLayer.NewsItems.Where(m => m.Status).ToList();
             if (ViewBag.NewsItems.Count > 0)
             {
                 ViewBag.NewsItems[0].first = true;
+            }
+
+            ViewBag.Sponsors = AccessLayer.Sponsors.Where(m => m.Status).ToList();
+            if (ViewBag.Sponsors.Count > 0)
+            {
+                ViewBag.Sponsors[0].first = true;
             }
 
             return View("Index");
