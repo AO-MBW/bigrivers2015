ae69841e0e9c6aeb4f0df3323fa1eb404c1508cb
rikproDVC <99016312@mydavinci.nl>
2015-06-09 22:01:54 +0200

    Updated all other views to have the line-paper background

--- src/Bigrivers.Client/Bigrivers.Client.WebApplication/Controllers/HomeController.cs
+++ src/Bigrivers.Client/Bigrivers.Client.WebApplication/Controllers/HomeController.cs
@@ -1,37 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Bigrivers.Client.WebApplication.ViewModels;
-
-// Via OData
-//using Bigrivers.Client.DAL.Bigrivers.Server.Model;
-//using Bigrivers.Client.DAL.Default;
-
-// Via Direct Reference
-using Bigrivers.Server.Data;
-using Bigrivers.Server.Model;
 
 namespace Bigrivers.Client.WebApplication.Controllers
 {
     public class HomeController : BaseController
     {
-        //Create AccessLayer with OData Uri from the App.Config
-        //static readonly Container AccessLayer = new Container(new Uri(ConfigurationManager.AppSettings["WebserviceUri"]));
-
-        //Create AccessLayer with direct reference to Server.Data
-        //static readonly BigriversDb AccessLayer = new BigriversDb();
-
         public ActionResult Index()
         {
-            ViewBag.ButtonItems = AccessLayer.ButtonItems.Where(m => m.Status).OrderBy(m => m.Order).ToList();
+            ViewBag.ButtonItems = AccessLayer.ButtonItems
+                .Where(m => m.Status)
+                .OrderBy(m => m.Order)
+                .ToList();
 
-            ViewBag.WidgetItems = AccessLayer.WidgetItems.Where(m => m.Status).OrderBy(m => m.Order).ToList();
+            ViewBag.WidgetItems = AccessLayer.WidgetItems
+                .Where(m => m.Status)
+                .OrderBy(m => m.Order)
+                .ToList();
 
-            ViewBag.Sponsors = AccessLayer.Sponsors.Where(m => m.Status).ToList();
+            ViewBag.Sponsors = AccessLayer.Sponsors
+                .Where(m => m.Status)
+                .ToList();
 
             return View("Index");
         }
@@ -40,93 +30,113 @@ namespace Bigrivers.Client.WebApplication.Controllers
         {
             if (id != null) return Event(id.Value);
 
-            ViewBag.EventList = AccessLayer.Events
+            var eventList = AccessLayer.Events
                 .Where(e => e.Status)
                 .ToList();
 
-            return View("Events");
+            return View("Events", eventList);
         }
 
         private ActionResult Event(int id)
         {
-            Event currentEvent = AccessLayer.Events
-                .Where(e => e.Id == id)
-                .SingleOrDefault();
+            var currentEvent = AccessLayer.Events
+                .SingleOrDefault(e => e.Id == id);
 
-            ViewBag.CurrentEvent = currentEvent;
-
-            if (currentEvent == null) return Redirect(Request.UrlReferrer.ToString());
+            if (currentEvent == null) return RedirectToAction("Events");
 
             ViewBag.PerformancesList = AccessLayer.Performances
                 .Include(p => p.Artist)
                 .Where(p => p.Event.Id == currentEvent.Id)
                 .ToList();
 
-            return View("Event");
+            return View("Event", currentEvent);
         }
 
         public ActionResult Performances(int? id)
         {
             if (id != null) return Performance(id.Value);
 
-            ViewBag.PerformancesList = AccessLayer.Performances
+            var performancesList = AccessLayer.Performances
                 .Where(p => p.Status)
                 .OrderBy(p => p.Start)
                 .ToList();
 
-            return View("Performances");
+            return View("Performances", performancesList);
         }
 
         private ActionResult Performance(int id)
         {
-            ViewBag.CurrentPerformance = AccessLayer.Performances
-                .Where(p => p.Id == id && p.Status)
-                .SingleOrDefault();
+            var currentPerformance = AccessLayer.Performances
+                .SingleOrDefault(p => p.Id == id && p.Status);
 
-            if (ViewBag.CurrentPerformance == null) return RedirectToAction("Performances");
+            if (currentPerformance == null) return RedirectToAction("Performances");
 
-            return View("Performance");
+            return View("Performance", currentPerformance);
         }
 
-        public ActionResult Genres(int? id)
-        {
-            if (id != null) return Genre(id.Value);
+        //public ActionResult Genres(int? id)
+        //{
+        //    if (id != null) return Genre(id.Value);
 
-            ViewBag.GenresList = AccessLayer.Genres.ToList();
+        //    ViewBag.GenresList = AccessLayer.Genres.ToList();
 
-            return View("Genres");
-        }
-        private ActionResult Genre(int id)
-        {
-            ViewBag.CurrentGenre = AccessLayer.Genres
-                .Where(g => g.Id == id)
-                .SingleOrDefault();
+        //    return View("Genres");
+        //}
+        //private ActionResult Genre(int id)
+        //{
+        //    ViewBag.CurrentGenre = AccessLayer.Genres
+        //        .Where(g => g.Id == id)
+        //        .SingleOrDefault();
 
-            if (ViewBag.CurrentGenre == null) return RedirectToAction("Genres");
+        //    if (ViewBag.CurrentGenre == null) return RedirectToAction("Genres");
 
-            return View("Genre");
-        }
+        //    return View("Genre");
+        //}
 
         public ActionResult Artists(int? id)
         {
             if (id != null) return Artist(id.Value);
 
-            ViewBag.ArtistsList = AccessLayer.Artists
+            var artistsList = AccessLayer.Artists
                 .Where(a => a.Status)
                 .ToList();
 
-            return View("Artists");
+            return View("Artists", artistsList);
         }
 
         private ActionResult Artist(int id)
         {
-            ViewBag.CurrentArtist = AccessLayer.Artists
-                .Where(a => a.Id == id)
-                .SingleOrDefault();
+            var currentArtist = AccessLayer.Artists
+                .SingleOrDefault(a => a.Id == id);
 
-            if (ViewBag.CurrentArtist == null) return RedirectToAction("Artists");
+            if (currentArtist == null) return RedirectToAction("Artists");
 
-            return View("Artist");
+            return View("Artist", currentArtist);
+        }
+
+        public ActionResult News(int? id)
+        {
+            if (id != null) return News(id.Value);
+
+            var newsItemsList = AccessLayer.NewsItems
+                .Where(a => a.Status)
+                .ToList();
+
+            return View("News", newsItemsList);
+        }
+
+        private ActionResult News(int id)
+        {
+            var newsItemsList = AccessLayer.NewsItems
+                .Where(a => a.Status)
+                .ToList();
+
+            var currentNews = newsItemsList
+                .SingleOrDefault(a => a.Id == id);
+
+            if (currentNews == null) return RedirectToAction("News");
+            ViewBag.NewsItemsList = newsItemsList;
+            return View("NewsItem", currentNews);
         }
 
         public ActionResult Contact()
@@ -150,31 +160,5 @@ namespace Bigrivers.Client.WebApplication.Controllers
 
             return View(model);
         }
-
-        public ActionResult News(int? id)
-        {
-            if (id != null) return News(id.Value);
-
-            ViewBag.NewsItemsList = AccessLayer.NewsItems
-                .Where(a => a.Status)
-                .ToList();
-
-            return View("News");
-        }
-
-        private ActionResult News(int id)
-        {
-            var newsItemsList = AccessLayer.NewsItems
-                .Where(a => a.Status)
-                .ToList();
-
-            var currentNews = newsItemsList
-                .SingleOrDefault(a => a.Id == id);
-
-            if (currentNews == null) return RedirectToAction("News");
-            ViewBag.NewsItemsList = newsItemsList;
-            return View("NewsItem", currentNews);
-        }
-
     }
 }
